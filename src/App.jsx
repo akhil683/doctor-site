606620c30f9973d67ac66dc8f6298ce0db2311ed
akhil683 <onkar.palsra@gmail.com>
2024-06-11 12:50:43 +0530

    optimized images & scrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop'
 
 const App = () => {
   return (
   <>
+  <ScrollToTop />
   <Navbar />
     <main>
       <Outlet />
